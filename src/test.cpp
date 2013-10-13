02cbfd65e7293df287b5b4149a08b17e2a19247d
Maarten van Gompel <proycon@anaproy.nl>
2013-10-13 06:37:24 +0200

    added hamlet

--- src/test.cpp
+++ src/test.cpp
@@ -374,19 +374,69 @@ int main( int argc, char *argv[] ) {
     cerr << endl; 
     
 
+    string rawcorpusfile = "/tmp/hamlet.txt";
+    ofstream * out = new ofstream(rawcorpusfile);
+    const char * poem = 
+    "To be or not to be, that is the question ;\n"
+    "Whether 'tis nobler in the mind to suffer\n"
+    "The slings and arrows of outrageous fortune ,\n"
+    "Or to take arms against a sea of troubles ,\n"
+    "And by opposing , end them . To die , to sleep ;\n"
+    "No more ; and by a sleep to say we end\n"
+    "The heart-ache and the thousand natural shocks\n"
+    "That flesh is heir to — 'tis a consummation\n"
+    "Devoutly to be wish'd . To die , to sleep ;\n"
+    "To sleep , perchance to dream . Ay , there's the rub ,\n"
+    "For in that sleep of death what dreams may come ,\n"
+    "When we have shuffled off this mortal coil ,\n"
+    "Must give us pause . There's the respect\n"
+    "That makes calamity of so long life ,\n"
+    "For who would bear the whips and scorns of time,\n"
+    "Th'oppressor's wrong , the proud man's contumely ,\n"
+    "The pangs of despised love , the law's delay ,\n"
+    "The insolence of office , and the spurns\n"
+    "That patient merit of th'unworthy takes ,\n"
+    "When he himself might his quietus make\n"
+    "With a bare bodkin? who would fardels bear ,\n"
+    "To grunt and sweat under a weary life ,\n"
+    "But that the dread of something after death ,\n"
+    "The undiscovered country from whose bourn\n"
+    "No traveller returns , puzzles the will ,\n"
+    "And makes us rather bear those ills we have\n"
+    "Than fly to others that we know not of?\n"
+    "Thus conscience does make cowards of us all ,\n"
+    "And thus the native hue of resolution\n"
+    "Is sicklied o'er with the pale cast of thought ,\n"
+    "And enterprises of great pitch and moment\n"
+    "With this regard their currents turn awry ,\n"
+    "And lose the name of action .\n"
+    "Soft you now! The fair Ophelia ! Nymph ,\n"
+    "in thy orisons be all my sins remember'd .\n";
+    *out << string(poem);
+    out->close();
+    cerr << "Class encoding corpus..." << endl;
+    system("colibri-classencode /tmp/hamlet.txt");
+
 
 
     PatternModelOptions options;
     options.DOREVERSEINDEX = true;
     options.DOFIXEDSKIPGRAMS = true;
 
+    cerr << "Building unindexed model" << endl;
     PatternModel<uint32_t> unindexedmodel;
-    std::string filename = "/tmp/data.colibri.patternmodel";
-    unindexedmodel.train(filename, options);
+    classfile = "/tmp/hamlet.colibri.cls";
+    std::string infilename = "/tmp/hamlet.colibri.dat";
+    std::string outputfilename = "/tmp/data.colibri.patternmodel";
+    unindexedmodel.train(infilename, options);
+    cerr << "Found " << unindexedmodel.types() << " types, " << unindexedmodel.tokens() << " tokens" << endl;
 
+
+    cerr << "Building nindexed model" << endl;
     IndexedPatternModel<> indexedmodel;
-    indexedmodel.types();
-    indexedmodel.train(filename, options);
+    indexedmodel.train(infilename, options);
+    cerr << "Found " << indexedmodel.types() << " types, " << indexedmodel.tokens() << " tokens" << endl;
 
 
 }
+
