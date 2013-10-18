029ec5d822c62e28814f955c9cec27ed5f3a4e95
Maarten van Gompel <proycon@anaproy.nl>
2013-10-18 13:09:33 +0200

    update

--- src/test.cpp
+++ src/test.cpp
@@ -382,115 +382,121 @@ int main( int argc, char *argv[] ) {
         cout << s8slice3.decode(classdecoder) << endl;
         cerr << endl; 
     }
-    
+        { 
 
-    string rawcorpusfile = "/tmp/hamlet.txt";
-    ofstream * out = new ofstream(rawcorpusfile);
-    const char * poem = 
-    "To be or not to be , that is the question ;\n"
-    "Whether 'tis nobler in the mind to suffer\n"
-    "The slings and arrows of outrageous fortune ,\n"
-    "Or to take arms against a sea of troubles ,\n"
-    "And by opposing , end them . To die , to sleep ;\n"
-    "No more ; and by a sleep to say we end\n"
-    "The heart-ache and the thousand natural shocks\n"
-    "That flesh is heir to — 'tis a consummation\n"
-    "Devoutly to be wish'd . To die , to sleep ;\n"
-    "To sleep , perchance to dream . Ay , there's the rub ,\n"
-    "For in that sleep of death what dreams may come ,\n"
-    "When we have shuffled off this mortal coil ,\n"
-    "Must give us pause . There's the respect\n"
-    "That makes calamity of so long life ,\n"
-    "For who would bear the whips and scorns of time,\n"
-    "Th'oppressor's wrong , the proud man 's contumely ,\n"
-    "The pangs of despised love , the law 's delay ,\n"
-    "The insolence of office , and the spurns\n"
-    "That patient merit of th' unworthy takes ,\n"
-    "When he himself might his quietus make\n"
-    "With a bare bodkin ? who would fardels bear ,\n"
-    "To grunt and sweat under a weary life ,\n"
-    "But that the dread of something after death ,\n"
-    "The undiscovered country from whose bourn\n"
-    "No traveller returns , puzzles the will ,\n"
-    "And makes us rather bear those ills we have\n"
-    "Than fly to others that we know not of ?\n"
-    "Thus conscience does make cowards of us all ,\n"
-    "And thus the native hue of resolution\n"
-    "Is sicklied o'er with the pale cast of thought ,\n"
-    "And enterprises of great pitch and moment\n"
-    "With this regard their currents turn awry ,\n"
-    "And lose the name of action .\n"
-    "Soft you now ! The fair Ophelia ! Nymph ,\n"
-    "in thy orisons be all my sins remember'd .\n"
-    "To flee or not to flee .\n" //additions to test skipgrams
-    "To see or not to see .\n"
-    "To pee or not to pee .\n"; //See that Shakespeare? I could be a poet too! 
-    *out << string(poem);
-    out->close();
+        string rawcorpusfile = "/tmp/hamlet.txt";
+        ofstream * out = new ofstream(rawcorpusfile);
+        const char * poem = 
+        "To be or not to be , that is the question ;\n"
+        "Whether 'tis nobler in the mind to suffer\n"
+        "The slings and arrows of outrageous fortune ,\n"
+        "Or to take arms against a sea of troubles ,\n"
+        "And by opposing , end them . To die , to sleep ;\n"
+        "No more ; and by a sleep to say we end\n"
+        "The heart-ache and the thousand natural shocks\n"
+        "That flesh is heir to — 'tis a consummation\n"
+        "Devoutly to be wish'd . To die , to sleep ;\n"
+        "To sleep , perchance to dream . Ay , there's the rub ,\n"
+        "For in that sleep of death what dreams may come ,\n"
+        "When we have shuffled off this mortal coil ,\n"
+        "Must give us pause . There's the respect\n"
+        "That makes calamity of so long life ,\n"
+        "For who would bear the whips and scorns of time,\n"
+        "Th'oppressor's wrong , the proud man 's contumely ,\n"
+        "The pangs of despised love , the law 's delay ,\n"
+        "The insolence of office , and the spurns\n"
+        "That patient merit of th' unworthy takes ,\n"
+        "When he himself might his quietus make\n"
+        "With a bare bodkin ? who would fardels bear ,\n"
+        "To grunt and sweat under a weary life ,\n"
+        "But that the dread of something after death ,\n"
+        "The undiscovered country from whose bourn\n"
+        "No traveller returns , puzzles the will ,\n"
+        "And makes us rather bear those ills we have\n"
+        "Than fly to others that we know not of ?\n"
+        "Thus conscience does make cowards of us all ,\n"
+        "And thus the native hue of resolution\n"
+        "Is sicklied o'er with the pale cast of thought ,\n"
+        "And enterprises of great pitch and moment\n"
+        "With this regard their currents turn awry ,\n"
+        "And lose the name of action .\n"
+        "Soft you now ! The fair Ophelia ! Nymph ,\n"
+        "in thy orisons be all my sins remember'd .\n"
+        "To flee or not to flee .\n" //additions to test skipgrams
+        "To see or not to see .\n"
+        "To pee or not to pee .\n"; //See that Shakespeare? I could be a poet too! 
+        *out << string(poem);
+        out->close();
 
-    cerr << "Class encoding corpus..." << endl;
-    system("colibri-classencode /tmp/hamlet.txt");
-
-
-
-    cerr << "Class decoding corpus..." << endl;
-    system("colibri-classdecode -c /tmp/hamlet.colibri.cls -f /tmp/hamlet.colibri.dat");
-
-
-    PatternModelOptions options;
-    options.DOREVERSEINDEX = true;
-    options.DOFIXEDSKIPGRAMS = true;
-
-    cerr << "Building unindexed model" << endl;
-    PatternModel<uint32_t> unindexedmodel;
-    const string classfile = "/tmp/hamlet.colibri.cls";
-    ClassDecoder classdecoder = ClassDecoder(classfile);
-    ClassEncoder classencoder = ClassEncoder(classfile);
-
-    cerr << endl;
-    std::string infilename = "/tmp/hamlet.colibri.dat";
-    std::string outputfilename = "/tmp/data.colibri.patternmodel";
-    unindexedmodel.train(infilename, options);
-    cerr << "Found " << unindexedmodel.size() << " patterns, " << unindexedmodel.types() << " types, " << unindexedmodel.tokens() << " tokens" << endl;
-    unindexedmodel.print(&std::cerr, classdecoder);
-    cerr << endl;
-    unindexedmodel.report(&std::cerr);
-    cerr << endl;
-    unindexedmodel.histogram(&std::cerr);
-
-
-    cerr << endl;
-    cerr << "Building indexed model" << endl;
-    IndexedPatternModel<> indexedmodel;
-    indexedmodel.train(infilename, options);
-    cerr << "Found " << indexedmodel.size() << " patterns, " << indexedmodel.types() << " types, " << indexedmodel.tokens() << " tokens" << endl;
-    indexedmodel.print(&std::cerr, classdecoder);
-    cerr << endl;
-    indexedmodel.report(&std::cerr);
-    cerr << endl;
-    indexedmodel.histogram(&std::cerr);
+        cerr << "Class encoding corpus..." << endl;
+        system("colibri-classencode /tmp/hamlet.txt");
 
 
 
- 	string querystring  = "To die , to sleep";
-	Pattern ngram = classencoder.input2pattern(querystring, true); 	
-    cerr << "Extracting subsumption relations for " << querystring << endl;
-
-    std::map<Pattern,int> relations = indexedmodel.getsubsumed(ngram);
-    indexedmodel.outputrelations(ngram, relations, classdecoder, &cerr,"SUBSUMES");
+        cerr << "Class decoding corpus..." << endl;
+        system("colibri-classdecode -c /tmp/hamlet.colibri.cls -f /tmp/hamlet.colibri.dat");
 
 
- 	string querystring2  = "not";
-    cerr << "Extracting subsumption relations for " << querystring2 << endl;
-	Pattern patternnot = classencoder.input2pattern(querystring2, true); 	
-    std::map<Pattern,int> relations2 = indexedmodel.getsubsumes(patternnot);
-    indexedmodel.outputrelations(patternnot, relations2, classdecoder, &cerr,"SUBSUMED-BY");
+        PatternModelOptions options;
+        options.DOREVERSEINDEX = true;
+        options.DOFIXEDSKIPGRAMS = true;
+
+        cerr << "Building unindexed model" << endl;
+        PatternModel<uint32_t> unindexedmodel;
+        const string classfile = "/tmp/hamlet.colibri.cls";
+        ClassDecoder classdecoder = ClassDecoder(classfile);
+        ClassEncoder classencoder = ClassEncoder(classfile);
+
+        cerr << endl;
+        std::string infilename = "/tmp/hamlet.colibri.dat";
+        std::string outputfilename = "/tmp/data.colibri.patternmodel";
+        unindexedmodel.train(infilename, options);
+        cerr << "Found " << unindexedmodel.size() << " patterns, " << unindexedmodel.types() << " types, " << unindexedmodel.tokens() << " tokens" << endl;
+        unindexedmodel.print(&std::cerr, classdecoder);
+        cerr << endl;
+        unindexedmodel.report(&std::cerr);
+        cerr << endl;
+        unindexedmodel.histogram(&std::cerr);
+
+        cerr << endl;
+        unindexedmodel.write(outputfilename);
 
 
-    std::map<Pattern,int> relations3 = indexedmodel.getleftneighbours(patternnot);
-    indexedmodel.outputrelations(patternnot, relations3, classdecoder, &cerr,"RIGHT-OF");
 
-    std::map<Pattern,int> relations4 = indexedmodel.getrightneighbours(patternnot);
-    indexedmodel.outputrelations(patternnot, relations4, classdecoder, &cerr,"LEFT-OF");
+        cerr << endl;
+        cerr << "Building indexed model" << endl;
+        IndexedPatternModel<> indexedmodel;
+        indexedmodel.train(infilename, options);
+        cerr << "Found " << indexedmodel.size() << " patterns, " << indexedmodel.types() << " types, " << indexedmodel.tokens() << " tokens" << endl;
+        indexedmodel.print(&std::cerr, classdecoder);
+        cerr << endl;
+        indexedmodel.report(&std::cerr);
+        cerr << endl;
+        indexedmodel.histogram(&std::cerr);
+
+
+
+        string querystring  = "To die , to sleep";
+        Pattern ngram = classencoder.input2pattern(querystring, true); 	
+        cerr << "Extracting subsumption relations for " << querystring << endl;
+
+        std::map<Pattern,int> relations = indexedmodel.getsubsumed(ngram);
+        indexedmodel.outputrelations(ngram, relations, classdecoder, &cerr,"SUBSUMES");
+
+
+        string querystring2  = "not";
+        cerr << "Extracting subsumption relations for " << querystring2 << endl;
+        Pattern patternnot = classencoder.input2pattern(querystring2, true); 	
+        std::map<Pattern,int> relations2 = indexedmodel.getsubsumes(patternnot);
+        indexedmodel.outputrelations(patternnot, relations2, classdecoder, &cerr,"SUBSUMED-BY");
+
+
+        std::map<Pattern,int> relations3 = indexedmodel.getleftneighbours(patternnot);
+        indexedmodel.outputrelations(patternnot, relations3, classdecoder, &cerr,"RIGHT-OF");
+
+        std::map<Pattern,int> relations4 = indexedmodel.getrightneighbours(patternnot);
+        indexedmodel.outputrelations(patternnot, relations4, classdecoder, &cerr,"LEFT-OF");
+
+    }
 }
 
