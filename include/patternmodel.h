db42411def687d8919b37ed49c4687e1a127174b
Maarten van Gompel <proycon@anaproy.nl>
2013-10-28 11:00:40 +0100

    update

--- include/patternmodel.h
+++ include/patternmodel.h
@@ -19,24 +19,7 @@ enum ModelType {
     INDEXEDPATTERNMODEL = 20,
 };
 
-int getmodeltype(const std::string filename) {
-    unsigned char null;
-    unsigned char model_type;
-    unsigned char model_version;
-    std::ifstream * f = new std::ifstream(filename.c_str());
-    f->read( (char*) &null, sizeof(char));        
-    f->read( (char*) &model_type, sizeof(char));        
-    f->read( (char*) &model_version, sizeof(char));        
-    f->close();
-    delete f;
-    if ((null != 0) || ((model_type != UNINDEXEDPATTERNMODEL) && (model_type != INDEXEDPATTERNMODEL) ))  {
-        return 0;
-    } else {
-        return model_type;
-    }
-}
-
-
+int getmodeltype(const std::string filename);
 
 
 class IndexedData {
---
+++ src/patternmodel.cpp
@@ -0,0 +1,18 @@
+#include <patternmodel.h>
+
+int getmodeltype(const std::string filename) {
+    unsigned char null;
+    unsigned char model_type;
+    unsigned char model_version;
+    std::ifstream * f = new std::ifstream(filename.c_str());
+    f->read( (char*) &null, sizeof(char));        
+    f->read( (char*) &model_type, sizeof(char));        
+    f->read( (char*) &model_version, sizeof(char));        
+    f->close();
+    delete f;
+    if ((null != 0) || ((model_type != UNINDEXEDPATTERNMODEL) && (model_type != INDEXEDPATTERNMODEL) ))  {
+        return 0;
+    } else {
+        return model_type;
+    }
+}
