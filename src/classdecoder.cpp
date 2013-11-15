43496d13dfa7c11420af2d2bdb148766cb70fec4
Maarten van Gompel <proycon@anaproy.nl>
2013-11-15 14:22:17 +0100

    cleanup

--- src/classdecoder.cpp
+++ src/classdecoder.cpp
@@ -163,45 +163,6 @@ void ClassDecoder::decodefile(const string & filename, unsigned int start, unsig
 	
 
 
-int readline(istream* IN, unsigned char* buffer, const int MAXBUFFERSIZE) {
-    int n = 0;
-    unsigned char prevc = 0;
-    short eolsequence = 0; //3 stages: 0 1 0 , when all three are found, we have a sentence
-    while (IN->good()) {
-        char bufchar;
-        IN->get(bufchar);
-        unsigned char c = (unsigned char) bufchar;
-        if ((c == 0) && (prevc == 0)) {
-        	//duplicate \0 bytes not allowed in corpus data (would imply null-length words), filtering out 
-       		continue;       
-        } else {
-        	prevc = c;
-        }        
-        if (n >= MAXBUFFERSIZE) {
-            cerr << "ERROR: Buffer overflow in classdecoder readline(): " << n << ". This indicates a sentence in the data that is far longer than anything reasonable, probably an error in sentence segmentation or tokenisation." << endl;
-        }
-        buffer[n] = c;        
-        if (c == 0) {
-            eolsequence++;
-            if (eolsequence == 3) return n - 2; //minus last two bytes of the 0 1 0 bytes (retaining final \0)
-        } else if (c == 1) {
-            if (eolsequence == 1) {
-                eolsequence++;
-            } else {
-                eolsequence = 0;
-            }
-        } else {
-            eolsequence = 0;
-        }
-        n++;
-    }    
-    if (IN->eof()) {
-    	return n;
-    } else {    
-    	return 0;
-    }
-}
-
 const int countwords(const unsigned char* data, const int l) {
 	if (l == 0) return 0;	
     int words = 1; 
