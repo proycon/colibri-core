38de5b85d2f8a726de242389a31b05c2a6a1e0b7
Maarten van Gompel <proycon@anaproy.nl>
2014-04-25 19:31:56 +0200

    fix

--- include/patternmodel.h
+++ include/patternmodel.h
@@ -1307,7 +1307,7 @@ class IndexedPatternModel: public PatternModel<IndexedData,IndexedDataHandler,Ma
         *OUT << "Total key bytesize in reverse index (references): " << ri_totalkeybs << " bytes (" << (ri_totalkeybs/1024/1024) << " MB)" << std::endl;
         *OUT << "Total value bytesize in reverse index (patterns): " << ri_totalvaluebs << " bytes (" << (ri_totalvaluebs/1024/1024) << " MB)" << std::endl;
 
-        const int t = (totalkeybs + totalvaluebs + ri_totalkeybs + ri_totalvaluebs);
+        const unsigned int t = (totalkeybs + totalvaluebs + ri_totalkeybs + ri_totalvaluebs);
         *OUT << "Total bytesize (without overhead): " << t << " bytes (" << (t/1024/1024) << " MB)" << std::endl;
     }
 
