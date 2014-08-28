12ae845707c9f8857d850073d5582e30266de9d0
Maarten van Gompel <proycon@anaproy.nl>
2014-08-28 22:56:39 +0200

    preparing to add modelcompare  tool

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ extensions = [ Extension("colibricore",
 
 setup(
     name = 'colibricore',
-    version = '0.5.4',
+    version = '0.5.5',
     ext_modules = extensions,
     cmdclass = {'build_ext': build_ext},
     packages=['colibricoreX'],
