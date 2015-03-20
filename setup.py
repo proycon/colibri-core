bd323da43baa67464c181faf93efce746b0bef91
Maarten van Gompel (proycon) <proycon@anaproy.nl>
2015-03-20 18:01:49 +0100

    Added compilation of C++ colibri-core stuff in setup.py, bit hacky...

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ from distutils.core import setup, Extension
 from Cython.Distutils import build_ext
 import glob
 import os
+import sys
 
 from os.path import expanduser
 HOMEDIR = expanduser("~")
+
+ROOTDIR = os.path.abspath(os.path.dirname(__file__))
 
 #cython's include is sucky unfortunately :( We'll have our own:
 for filename in glob.glob("*.in.pyx"):
@@ -22,11 +25,77 @@ for filename in glob.glob("*.in.pyx"):
                             f_out.write(" " * found + incline)
 
 
+
+#not the most elegant hack, but we're going to try compile colibri-core here before we continue with the rest:
+
+#defaults:
+includedirs = ["/usr/include/colibri-core", "/usr/include/libxml2"]
+libdirs = ["/usr/lib"]
+if ('install' in sys.argv[1:] or 'build_ext' in sys.argv[1:]) and not '--help' in sys.argv[1:]:
+    if '-n' in sys.argv[1:]:
+        print("Dry run not supported for colibri-compilation",file=sys.stderr)
+        sys.exit(2)
+
+    os.chdir(ROOTDIR)
+
+    #see if we got a prefix:
+    prefix = None
+    for i, arg in enumerate(sys.argv):
+        if i == 0: continue
+        if arg == "--prefix" or arg == "--install-base":
+            prefix = sys.argv[i+1]
+        elif arg == "--root":
+            prefix = sys.argv[i+1] + '/usr/'
+        elif arg[:15] == "--install-base=":
+            prefix = arg[15:]
+        elif arg[:9] == "--prefix=":
+            prefix = arg[9:]
+        elif arg[:7] == "--root=":
+            prefix = sys.argv[i+1] + arg[7:]
+        elif arg == "--user":
+            prefix = HOMEDIR + "/.local/"
+
+    if 'VIRTUAL_ENV' in os.environ and not prefix:
+        prefix = os.environ['VIRTUAL_ENV']
+
+    if not os.path.exists(ROOTDIR + "/configure") or '-f' in sys.argv[1:] or '--force' in sys.argv[1:]:
+        print("Bootstrapping colibri-core",file=sys.stderr)
+        r = os.system("bash bootstrap")
+        if r != 0:
+            print("Bootstrapping colibri-core failed: make sure you have autoconf, automake and autoconf-archive installed?",file=sys.stderr)
+            sys.exit(2)
+    if not os.path.exists(ROOTDIR + "/Makefile") or '-f' in sys.argv[1:] or '--force' in sys.argv[1:]:
+        if prefix:
+            r = os.system("./configure --prefix=" + prefix)
+        else:
+            r = os.system("./configure")
+        if r != 0:
+            print("Configure of colibri-core failed",file=sys.stderr)
+            sys.exit(2)
+    r = os.system("make")
+    if r != 0:
+        print("Make of colibri-core failed",file=sys.stderr)
+        sys.exit(2)
+    r = os.system("make install")
+    if r != 0:
+        print("Make install of colibri-core failed",file=sys.stderr)
+        sys.exit(2)
+
+    if prefix:
+        includedirs += [prefix + "/include/colibri-core", prefix + "/include", prefix + "/include/libxml2"]
+        libdirs += [prefix + "/lib"]
+
+
+
+
+
+
+
 extensions = [ Extension("colibricore",
                 ["unordered_map.pxd", "colibricore_classes.pxd", "colibricore_wrapper.pyx"],
                 language='c++',
-                include_dirs=[HOMEDIR + '/local/include/colibri-core/', HOMEDIR + '/local/include/','/usr/include/', '/usr/include/libxml2','/usr/local/include/' ],
-                library_dirs=[HOMEDIR + '/local/lib/','/usr/lib','/usr/local/lib'],
+                include_dirs=includedirs,
+                library_dirs=libdirs,
                 libraries=['colibricore'],
                 extra_compile_args=['--std=c++0x'],
                 pyrex_gdb=True
@@ -34,7 +103,7 @@ extensions = [ Extension("colibricore",
 
 setup(
     name = 'colibricore',
-    version = '0.5.7',
+    version = '0.5.7.1',
     ext_modules = extensions,
     cmdclass = {'build_ext': build_ext},
     classifiers=[
