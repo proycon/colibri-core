5097b48c4d1828824f1993bccfc34cd3cdf785c1
Maarten van Gompel <proycon@anaproy.nl>
2015-08-11 16:44:32 +0200

    Revert "more explicit typedef so cython doesn't get confused"
    This reverts commit d574ff5ae7cb2526399340762335dc61462dd9c4.

--- include/patternmodel.h
+++ include/patternmodel.h
@@ -180,8 +180,8 @@ class PatternModelOptions {
 
 };
 
-typedef PatternMap<uint32_t,BaseValueHandler<uint32_t>,uint64_t> t_relationmap;
-typedef PatternMap<double,BaseValueHandler<double>,uint64_t> t_relationmap_double;
+typedef PatternMap<uint32_t> t_relationmap;
+typedef PatternMap<double> t_relationmap_double;
 
 /**
  * Basic read-only interface for pattern models, abstract base class.
