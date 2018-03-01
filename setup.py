3da3fc41424a36744203536cedf62d6142849ebb
Maarten van Gompel <proycon@anaproy.nl>
2018-03-01 11:38:37 +0100

    another version bump, premature release of 2.4.7 with inconsistent numbering

--- setup.py
+++ setup.py
@@ -177,7 +177,7 @@ setup(
     license = "GPL",
     keywords = "nlp computational_linguistics frequency ngram skipgram pmi cooccurrence linguistics",
     long_description=read('README.rst'),
-    version = '2.4.7',
+    version = '2.4.8',
     ext_modules = extensions,
     cmdclass = {'build_ext': build_ext},
     classifiers=[
