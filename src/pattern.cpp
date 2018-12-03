133226c90cae4e2557cc99a1f32c84553e24a562
Maarten van Gompel <proycon@anaproy.nl>
2018-12-03 22:07:01 +0100

    Implemented extra sanity check in IndexedCorpus loading #41

--- src/pattern.cpp
+++ src/pattern.cpp
@@ -1860,6 +1860,10 @@ void IndexedCorpus::load(std::istream *in, bool debug) {
         prevhigh = (*cursor >= 128);
         cursor++;
     }
+    if (sentence != sentenceindex.size()) {
+        std::cerr << "ERROR: IndexedCorpus expected " << sentence << " sentence, but loaded only " << sentenceindex.size() << "!! Max size is " << sentenceindex.max_size() << std::endl;
+        throw InternalError();
+    }
     if (debug) cerr << "Loaded " << sentenceindex.size() << " sentences" << endl;
 	patternpointer = new PatternPointer(corpus,corpussize);
 }
