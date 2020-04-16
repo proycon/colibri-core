75e852716b97273ad743d7e03d0e00cbcae6ceb6
Maarten van Gompel <proycon@anaproy.nl>
2020-04-16 13:40:10 +0200

    version bump

--- setup.py
+++ setup.py
@@ -183,7 +183,7 @@ setup(
     license = "GPL",
     keywords = "nlp computational_linguistics frequency ngram skipgram pmi cooccurrence linguistics",
     long_description=read('README.rst'),
-    version = '2.5.4',
+    version = '2.5.5',
     ext_modules = extensions,
     cmdclass = {'build_ext': build_ext},
     classifiers=[
